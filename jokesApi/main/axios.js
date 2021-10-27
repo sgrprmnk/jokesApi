922a187f5b22b7244d1c244347feec66fd9fc62f
Sagar Pramanik <sgrprmnk@gmail.com>
2021-10-27 21:08:18 +0530

    Add files via upload

---
+++ jokesApi/main/axios.js
@@ -0,0 +1,43 @@
+// axios
+//   .get("https://api.cryptonator.com/api/ticker/btc-usd")
+//   .then((res) => {
+//     console.log(res.data.ticker.price);
+//   })
+//   .catch((err) => {
+//     console.log("ERROR!", err);
+//   });
+
+const fetchBitcoinPrice = async () => {
+  try {
+    const res = await axios.get(
+      "https://api.cryptonator.com/api/ticker/btc-usd"
+    );
+    console.log(res.data.ticker.price);
+  } catch (e) {
+    console.log("ERROR!", e);
+  }
+};
+
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
+
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke();
+  //console.log(jokeText);
+  const newLI = document.createElement("LI");
+  newLI.append(jokeText);
+  jokes.append(newLI);
+};
+
+const getDadJoke = async () => {
+  try {
+    const config = { headers: { Accept: "application/json" } };
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
+    //   console.log(res.data.joke);
+    return res.data.joke;
+  } catch (e) {
+    return "No jokes Available! Sorry";
+  }
+};
+
+button.addEventListener("click", addNewJoke);
